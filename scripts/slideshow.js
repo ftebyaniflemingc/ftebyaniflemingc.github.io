385eae68f0a627587e8a6e8c12ef4b7d696217e5
Kamel Chehboun <kchehbou@flemingcollege.ca>
2019-11-27 15:47:59 -0500

    JS Correction

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -5,7 +5,7 @@ carousel();
 
 function carousel() {
     var i;
-    var x = document.getElementsByClassName("myslides");
+    var x = document.getElementsByClassName("slidesshow");
     for (i = 0; i < x.length; i++) {
        x[i].style.display = "none";  
     }
