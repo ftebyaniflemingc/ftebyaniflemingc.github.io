2e33c310e2519e9048162cf428d0b6edf2ee7dd0
ftebyaniflemingc <57412925+ftebyaniflemingc@users.noreply.github.com>
2020-06-02 14:52:14 -0400

    Update datalayer.js

--- durham_housing/app/datalayer.js
+++ durham_housing/app/datalayer.js
@@ -16,25 +16,19 @@
         zoom: 11           
       });
 
-        // CensusTracs feature layer (polygon)
+        // CensusTracs feature Year 2019 layer (polygon)
       var year19Layer = new FeatureLayer({
       url: "https://services1.arcgis.com/pMeXRvgWClLJZr3s/arcgis/rest/services/South_Durham_Region/FeatureServer/0"
       });
 
-  //    map.add(year19Layer);
+      map.add(year19Layer);
       
-      //  feature layer (polygon)
-    //  var trailsLayer = new FeatureLayer({
-//        url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails/FeatureServer/0"
-//      });
+        // CensusTracs feature Year 2019 layer (polygon)
+      var year1Layer = new FeatureLayer({
+      url: "https://services1.arcgis.com/pMeXRvgWClLJZr3s/arcgis/rest/services/South_Durham_Region/FeatureServer/1"
+      });
 
-//      map.add(trailsLayer, 0);
-
-      // Parks and open spaces (polygons)
-//      var parksLayer = new FeatureLayer({
-      //  url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Parks_and_Open_Space/FeatureServer/0"
-//      });
-
-  //    map.add(parksLayer, 0);
+      map.add(year18Layer);
+    
 
     });
