4621bb58194f1b40e74814f2addcf1bee9fd5497
christinelacanilao <57382843+christinelacanilao@users.noreply.github.com>
2020-06-02 17:46:20 -0400

    Update datalayer.js
    Need to adjust client-side query to apply for our data layers

--- durham_housing/app/datalayer.js
+++ durham_housing/app/datalayer.js
@@ -80,3 +80,43 @@
       map.add(year10Layer);
 
   });
+
+// Query Feature Layer
+
+// Client-side queries 
+      function queryFeatureLayerView(point, distance, spatialRelationship, sqlExpression) {
+        // Add the layer if it is missing
+        if (!map.findLayerById(featureLayer.id)) {
+          featureLayer.outFields = ["*"];
+          map.add(featureLayer,0);
+        }
+
+        / Set up the query
+        var query = {
+          geometry: point,
+          distance: distance,
+          spatialRelationship: spatialRelationship,
+          outFields: ["*"],
+          returnGeometry: true,
+          where: sqlExpression
+        };
+        // Wait for the layerview to be ready and then query features
+        view.whenLayerView(featureLayer).then(function(featureLayerView) {
+          if (featureLayerView.updating) {
+            var handle = featureLayerView.watch("updating", function(isUpdating){
+              if (!isUpdating) {
+                // Execute the query
+                featureLayerView.queryFeatures(query).then(function(result) {
+                  addGraphics(result)
+                });
+                handle.remove();
+              }
+            });
+          } else {
+            // Execute the query
+            featureLayerView.queryFeatures(query).then(function(result) {
+              addGraphics(result);
+            });
+          }
+        });
+      }
