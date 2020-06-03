3e55ad7b4c2b007fa054fe7f200c189802218633
ftebyaniflemingc <57412925+ftebyaniflemingc@users.noreply.github.com>
2020-06-03 14:00:59 -0400

    Update datalayer.js

--- durham_housing/app/datalayer.js
+++ durham_housing/app/datalayer.js
@@ -2,7 +2,7 @@
 require([
       "esri/Map",
       "esri/views/MapView",
-      "esri/layers/FeatureLayer".
+      "esri/layers/FeatureLayer",
       "esri/geometry/Extent"
       //"esri/SpatialReference"
     //  "esri/widgets/Legend", //to define Legend
@@ -22,15 +22,15 @@ require([
       var view = new MapView({
         container: "mapid",  
         map: webmap,
-        center: [-78.871866,43.914656],
+        //center: [-78.871866,43.914656],
         zoom: 10
       
             //set the extent on the view
-          var extent = new Extent({
-            xmin: -78.510,
-            ymin: 43.840,
-            xmax: -79.270,
-            ymax: 44.076,
+          var extent = new esri.geometry.Extent({
+            xmax: -78.510,
+            ymax: 43.840,
+            xmin: -79.270,
+            ymin: 44.076,
        // spatialReference: new SpatialReference ({ "wkid": 32617 })
       });
    });
