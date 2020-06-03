00e8f33d61433c9effde7ef3cc5535a05ce9247e
ftebyaniflemingc <57412925+ftebyaniflemingc@users.noreply.github.com>
2020-06-03 17:00:11 -0400

    Update datalayer.js

--- durham_housing/app/datalayer.js
+++ durham_housing/app/datalayer.js
@@ -24,7 +24,7 @@ require([
         });
             //set the extent on the view
         var extent = new Extent({
-            -78.510, ymax: 43.840, xmin: -79.270, ymin: 44.076
+           xmax: -78.510, ymax: 43.840, xmin: -79.270, ymin: 44.076
        // spatialReference: new SpatialReference ({ "wkid": 32617 })
    //   });
    });
