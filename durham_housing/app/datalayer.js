0fdcca1372ff193c8b596c97a0a30cef657e9865
ftebyaniflemingc <57412925+ftebyaniflemingc@users.noreply.github.com>
2020-06-06 13:30:29 -0400

    Update datalayer.js

--- durham_housing/app/datalayer.js
+++ durham_housing/app/datalayer.js
@@ -32,7 +32,7 @@ require([
     //     {id: 5, title: "Year2014", visible: true}, 
      //    {id: 6, title: "Year2013", visible: true}, 
     //     {id: 7, title: "Year2012", visible: true}, 
-    //     {id: 8, title: "Year2011", visible: true}, 
+         {id: 8, title: "Year2011", visible: true}, 
          {id: 9, title: "Year2010", visible: true}]
   });
 
