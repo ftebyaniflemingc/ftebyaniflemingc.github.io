37547c25d2c9bba226069e4d9ba3ef44ad24597f
ftebyaniflemingc <57412925+ftebyaniflemingc@users.noreply.github.com>
2020-06-06 12:26:18 -0400

    Update datalayer.js

--- durham_housing/app/datalayer.js
+++ durham_housing/app/datalayer.js
@@ -18,12 +18,7 @@ require([
     
   //Web Map  set up
    var webmap = new WebMap({  
-         basemap: { portalItem: { 
-                    id: "3582b744bba84668b52a16b0b6942544"
-               //   id: "a37abf36893f42bbaccb0cef64fb28ab"
-              }
-            },
-
+         
   });   
  
 /*             
@@ -118,12 +113,11 @@ require([
         //using web map by Esri as basemap
         var myview = new MapView({
           webmap: new WebMap({
-           // basemap: { portalItem: { 
-            //        id: "3582b744bba84668b52a16b0b6942544"}},
+         basemap: { portalItem: { 
+                    id: "3582b744bba84668b52a16b0b6942544"
                //   id: "a37abf36893f42bbaccb0cef64fb28ab"
-              
-            
-                 
+              }
+            },          
       //lys: [year19Layer, year18Layer, year17Layer, year16Layer, year15Layer, year14Layer, year13Layer, year12Layer, year11Layer, year10Layer]
    layers: [layer]       
           }),
