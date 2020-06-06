c80ce78e10631aa4826fb32ff6cbfad5ae6d322c
ftebyaniflemingc <57412925+ftebyaniflemingc@users.noreply.github.com>
2020-06-06 14:58:02 -0400

    Update datalayer.js

--- durham_housing/app/datalayer.js
+++ durham_housing/app/datalayer.js
@@ -33,7 +33,7 @@ require([
                   content: "The census boundary has {infilling2010_csv_SumOfUnits} housing starts."   // Display ttext in pop-up
             },
              sublayers:[
-         {id: 0, title: "Year2019", visible: true, renderer: Year2019Rebderer}, 
+         {id: 0, title: "Year2019", visible: true, renderer: "Year2019Rebderer"}, 
          {id: 1, title: "Year2018", visible: true, renderer: "Year2019Rebderer"}, 
          {id: 2, title: "Year2017", visible: true}, 
          {id: 3, title: "Year2016", visible: true}, 
