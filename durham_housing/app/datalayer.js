cf7caf5f4ecf480a81ae463acfb25896d86d4744
ftebyaniflemingc <57412925+ftebyaniflemingc@users.noreply.github.com>
2020-06-07 19:24:35 -0400

    Update datalayer.js

--- durham_housing/app/datalayer.js
+++ durham_housing/app/datalayer.js
@@ -109,9 +109,9 @@ webmap.add(layer);
   // Mapview, referencing WebMap instance
       var myview = new MapView({
             map: webmap,  // The WebMap instance created above
-           // ({ basemap:{  portalItem:{id: "3582b744bba84668b52a16b0b6942544"}},                
-               layers: [layer]
-            }),
+           // ({ basemap:{  portalItem:{id: "3582b744bba84668b52a16b0b6942544"}}}),
+               layers: [layer],
+            
             container: "mapid",
             center: [-78.871866,43.914656],
             zoom: 10
