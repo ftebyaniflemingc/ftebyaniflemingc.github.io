8045df1647685000fb863dd7927c73ec79343593
ftebyaniflemingc <57412925+ftebyaniflemingc@users.noreply.github.com>
2020-06-07 13:34:30 -0400

    Update datalayer.js

--- durham_housing/app/datalayer.js
+++ durham_housing/app/datalayer.js
@@ -34,7 +34,8 @@ require([
      
    
   //---------------FeatureLayers---------------
-        
+           
+      
   var layer = new FeatureLayer({ 
          portalItem: { // autocasts as new PortalItem()
          id: "2f46a0d5c31f4f5fb0d2d8f53eb9998a",
@@ -44,81 +45,123 @@ require([
   
         {id: 0, title: "Year2019", visible: true, 
          definitionExpression: "(CensusBoundary2016_CT> 0) AND (infilling2019_csv_Census_Tract > 0)",         
-         popupTemplate: {        // Enable a popup
+         /*popupTemplate: {        // Enable a popup
                   title: "{CensusBoundary2016_CTNAME}",       // Show attribute value
                   content: "The census boundary has {infilling2019_csv_SumOfUnits} housing starts."   // Display text in pop-up
-            }
+            }*/
          }, 
          {id: 1, title: "Year2018", visible: true, 
           definitionExpression: "(CensusBoundary2016_CT> 0) AND (infilling2018_csv_Census_Tract > 0)",
-                  popupTemplate: {        // Enable a popup
+                  /*popupTemplate: {        // Enable a popup
                   title: "{CensusBoundary2016_CTNAME}",       // Show attribute value
                   content: "The census boundary has {infilling2018_csv_SumOfUnits} housing starts."   // Display ttext in pop-up
-            }
+            }*/
          }, 
          {id: 2, title: "Year2017", visible: true, 
           definitionExpression: "(CensusBoundary2016_CT> 0) AND (infilling2017_csv_Census_Tract > 0)",
-                  popupTemplate: {        // Enable a popup
+             /*    popupTemplate: {        // Enable a popup
                   title: "{CensusBoundary2016_CTNAME}",       // Show attribute value
                   content: "The census boundary has {infilling2017_csv_SumOfUnits} housing starts."   // Display ttext in pop-up
-            }
+            }*/
          }, 
          {id: 3, title: "Year2016", visible: true, 
           definitionExpression: "(CensusBoundary2016_CT> 0) AND (infilling2016_csv_Census_Tract > 0)",
-                 popupTemplate: {        // Enable a popup
+               /*  popupTemplate: {        // Enable a popup
                   title: "{CensusBoundary2016_CTNAME}",       // Show attribute value
                   content: "The census boundary has {infilling2016_csv_SumOfUnits} housing starts."   // Display ttext in pop-up
-            }
+            }*/
          }, 
          {id: 4, title: "Year2015",  visible: true, 
           definitionExpression: "(CensusBoundary2016_CT> 0) AND (infilling2015_csv_Census_Tract > 0)",
-                 popupTemplate: {        // Enable a popup
+                /* popupTemplate: {        // Enable a popup
                   title: "{CensusBoundary20166CTNAME}",       // Show attribute value
                   content: "The census boundary has {infilling2015_csv_SumOfUnits} housing starts."   // Display ttext in pop-up
-            }
+            }*/
          }, 
          {id: 5, title: "Year2014",  visible: true, 
           definitionExpression: "(CensusBoundary2016_CT> 0) AND (infilling2014_csv_Census_Tract > 0)",
-                  popupTemplate: {        // Enable a popup
+        /*          popupTemplate: {        // Enable a popup
                   title: "{CensusBoundary2014_CTNAME}",       // Show attribute value
                   content: "The census boundary has {infilling2014_csv_SumOfUnits} housing starts."   // Display ttext in pop-up
-            }
+            }*/
          }, 
          {id: 6, title: "Year2013",  visible: true, 
           definitionExpression: "(CensusBoundary2016_CT> 0) AND (infilling2013_csv_Census_Tract > 0)",
-                 popupTemplate: {        // Enable a popup
+               /*  popupTemplate: {        // Enable a popup
                   title: "{CensusBoundary20166CTNAME}",       // Show attribute value
                   content: "The census boundary has {infilling2013_csv_SumOfUnits} housing starts."   // Display ttext in pop-up
-            }
+            }*/
          }, 
          {id: 7, title: "Year2012",  visible: true, 
           definitionExpression: "(CensusBoundary2016_CT> 0) AND (infilling2012_csv_Census_Tract > 0)",
-                 popupTemplate: {        // Enable a popup
+            /*     popupTemplate: {        // Enable a popup
                   title: "{CensusBoundary2012_CTNAME}",       // Show attribute value
                   content: "The census boundary has {infilling2016_csv_SumOfUnits} housing starts."   // Display ttext in pop-up
-            }
+            }*/
          }, 
          {id: 8, title: "Year2011",  visible: true, 
           definitionExpression: "(CensusBoundary2011_CT> 0) AND (infilling2011_csv_Census_Tract > 0)",
-                 popupTemplate: {        // Enable a popup
+               /*  popupTemplate: {        // Enable a popup
                   title: "{CensusBoundary2011_CTNAME}",       // Show attribute value
                   content: "The census boundary has {infilling2011_csv_SumOfUnits} housing starts."   // Display ttext in pop-up
-            }},
+            }*/
+            },
          {id: 9, title: "Year2010",  visible: true, 
           definitionExpression: "(CensusBoundary2016_CT> 0) AND (infilling2010_csv_Census_Tract > 0)",
-                 popupTemplate: {        // Enable a popup
+               /*  popupTemplate: {        // Enable a popup
                   title: "{CensusBoundary2016_CTNAME}",       // Show attribute value
                   content: "The census boundary has {infilling2010_csv_SumOfUnits} housing starts."   // Display ttext in pop-up
-            }
+            }*/
          }
          ],
          }
         
 
   }); //FeatureLayer
+      
 webmap.add(layer);
       
-      
+ var template = {
+        // autocasts as new PopupTemplate()
+        title: "{CensusBoundary2016_CTNAME}", // Show attribute value
+        content: [
+          {
+            type: "text",
+            text:
+              "The census boundary has {infilling2019_csv_SumOfUnits} housing starts."   // Display text in pop-up
+          },
+          {
+            type: "fields",
+            fieldInfos: [
+              {
+                fieldName: "Census_Tract",
+                label: "The Census Tract Per Year",
+                format: {
+                  digitSeparator: true,
+                  places: 0
+                }
+              },
+              {
+                fieldName: "SumOfUnits",
+                label: "Som of Units Starts in the Year",
+                format: {
+                  digitSeparator: true,
+                  places: 0
+                }
+              },
+              {
+                fieldName: "expression/lethality-arcade",
+                format: {
+                  digitSeparator: true,
+                  places: 0
+                }
+              }
+            ]
+          }
+        ],
+        expressionInfos: arcadeExpressionInfos
+      };  
+       layer.popupTemplate = template;
       //---------------Legend---------------
       
         const mylegend = new Expand({
