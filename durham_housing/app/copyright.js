52f6baa7ed85b7e3546a189997d4e606e6a39c0d
ftebyaniflemingc <57412925+ftebyaniflemingc@users.noreply.github.com>
2020-06-07 18:18:12 -0400

    Create copyright.js

---
+++ durham_housing/app/copyright.js
@@ -0,0 +1,4 @@
+// Footer, new copyright year
+// Sourced from http://updateyourfooter.com/
+// Referenced from https://www.digitalocean.com/community/tutorials/how-to-add-javascript-to-html
+document.write(new Date().getFullYear());
