32101934cd7e837c8931047801414fba516d76ec
ftebyaniflemingc <57412925+ftebyaniflemingc@users.noreply.github.com>
2020-06-07 12:02:20 -0400

    Update datalayer.js

--- durham_housing/app/datalayer.js
+++ durham_housing/app/datalayer.js
@@ -56,47 +56,55 @@ require([
             }
          }, 
          {id: 2, title: "Year2017", visible: true, 
+          definitionExpression: "(CensusBoundary2016_CT> 0) AND (infilling2017_csv_Census_Tract > 0)",
                   popupTemplate: {        // Enable a popup
                   title: "{CensusBoundary2016_CTNAME}",       // Show attribute value
                   content: "The census boundary has {infilling2017_csv_SumOfUnits} housing starts."   // Display ttext in pop-up
             }
          }, 
          {id: 3, title: "Year2016", visible: true, 
+          definitionExpression: "(CensusBoundary2016_CT> 0) AND (infilling2016_csv_Census_Tract > 0)",
                  popupTemplate: {        // Enable a popup
                   title: "{CensusBoundary2016_CTNAME}",       // Show attribute value
                   content: "The census boundary has {infilling2016_csv_SumOfUnits} housing starts."   // Display ttext in pop-up
             }
          }, 
          {id: 4, title: "Year2015",  visible: true, 
+          definitionExpression: "(CensusBoundary2016_CT> 0) AND (infilling2015_csv_Census_Tract > 0)",
                  popupTemplate: {        // Enable a popup
                   title: "{CensusBoundary20166CTNAME}",       // Show attribute value
                   content: "The census boundary has {infilling2015_csv_SumOfUnits} housing starts."   // Display ttext in pop-up
             }
          }, 
          {id: 5, title: "Year2014",  visible: true, 
+          definitionExpression: "(CensusBoundary2016_CT> 0) AND (infilling2014_csv_Census_Tract > 0)",
                   popupTemplate: {        // Enable a popup
                   title: "{CensusBoundary2014_CTNAME}",       // Show attribute value
                   content: "The census boundary has {infilling2014_csv_SumOfUnits} housing starts."   // Display ttext in pop-up
             }
          }, 
          {id: 6, title: "Year2013",  visible: true, 
+          definitionExpression: "(CensusBoundary2016_CT> 0) AND (infilling2013_csv_Census_Tract > 0)",
                  popupTemplate: {        // Enable a popup
                   title: "{CensusBoundary20166CTNAME}",       // Show attribute value
                   content: "The census boundary has {infilling2013_csv_SumOfUnits} housing starts."   // Display ttext in pop-up
             }
          }, 
          {id: 7, title: "Year2012",  visible: true, 
+          definitionExpression: "(CensusBoundary2016_CT> 0) AND (infilling2012_csv_Census_Tract > 0)",
                  popupTemplate: {        // Enable a popup
                   title: "{CensusBoundary2012_CTNAME}",       // Show attribute value
                   content: "The census boundary has {infilling2016_csv_SumOfUnits} housing starts."   // Display ttext in pop-up
             }
          }, 
          {id: 8, title: "Year2011",  visible: true, 
+          definitionExpression: "(CensusBoundary2011_CT> 0) AND (infilling2011_csv_Census_Tract > 0)",
                  popupTemplate: {        // Enable a popup
                   title: "{CensusBoundary2011_CTNAME}",       // Show attribute value
                   content: "The census boundary has {infilling2011_csv_SumOfUnits} housing starts."   // Display ttext in pop-up
             }},
          {id: 9, title: "Year2010",  visible: true, 
+          definitionExpression: "(CensusBoundary2016_CT> 0) AND (infilling2010_csv_Census_Tract > 0)",
                  popupTemplate: {        // Enable a popup
                   title: "{CensusBoundary2016_CTNAME}",       // Show attribute value
                   content: "The census boundary has {infilling2010_csv_SumOfUnits} housing starts."   // Display ttext in pop-up
@@ -104,6 +112,8 @@ require([
          }
          ],
          }
+        renderer: renderer
+
   }); //FeatureLayer
 webmap.add(layer);
       
