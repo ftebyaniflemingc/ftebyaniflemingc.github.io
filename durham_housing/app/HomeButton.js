3e56e309dd22451e33abfd44a537213dc116a1ab
ftebyaniflemingc <57412925+ftebyaniflemingc@users.noreply.github.com>
2020-06-11 19:58:32 -0400

    Update HomeButton.js

--- durham_housing/app/HomeButton.js
+++ durham_housing/app/HomeButton.js
@@ -15,7 +15,7 @@ require([
       "dojo/domReady!"
         ], 
         function(WebMap, MapView, FeatureLayer, Layer, Home, Fullscreen, LayerList, Legend, Expand, TimeSlider, PopupTemplate ){
-    
+    let layerView;
       //---------------FeatureLayers---------------
    /// Creates a WebMap instance
       var webmap = new WebMap({
@@ -204,7 +204,7 @@ webmap.add(layer);
         const mylegend = new Expand({
           content: new Legend({
             view: myview,
-            style: 'card' // other styles include 'classic'
+            style: 'classic' // other styles include 'classic'
           }),
           view: myview,
           expanded: true,
@@ -212,6 +212,8 @@ webmap.add(layer);
         myview.ui.add(mylegend, "bottom-left");
       
      //---------------Time Slider--------------- 
+      
+      /*
     const mytimeSlider = new TimeSlider({
           container: "timeSlider",
           mode: "time-window",
@@ -307,5 +309,61 @@ const fullTimeExtent = layer.timeInfo.fullTimeExtent;
                         });
          timeSlider.play();  
 */
+      const timeSlider = new TimeSlider({
+          container: "timeSlider",
+          playRate: 50,
+          stops: {
+            interval: {
+              value: 1,
+              unit: "hours"
+            }
+          }
+        });
+        myview.ui.add(timeSlider, "manual");
+
+        // wait till the layer view is loaded
+        myview.whenLayerView(layer).then(function(lv) {
+          layerView = lv;
+
+          // start time of the time slider - 5/25/2019
+          const start = new Date(2009, 12, 31);
+          // set time slider's full extent to
+          // 5/25/5019 - until end date of layer's fullTimeExtent
+          timeSlider.fullTimeExtent = {
+            start: start,
+            end: layer.timeInfo.fullTimeExtent.end
+          };
+
+          // We will be showing earthquakes with one day interval
+          // when the app is loaded we will show earthquakes that
+          // happened between 5/25 - 5/26.
+          const end = new Date(start);
+          // end of current time extent for time slider
+          // showing earthquakes with one day interval
+          end.setDate(end.getDate() + 1);
+
+          // Values property is set so that timeslider
+          // widget show the first day. We are setting
+          // the thumbs positions.
+          timeSlider.values = [start, end];
+        });
+
+        // watch for time slider timeExtent change
+        timeSlider.watch("timeExtent", function() {
+          // only show earthquakes happened up until the end of
+          // timeSlider's current time extent.
+          layer.definitionExpression =
+            "time <= " + timeSlider.timeExtent.end.getTime();
+
+          // now gray out earthquakes that happened before the time slider's current
+          // timeExtent... leaving footprint of earthquakes that already happened
+          layerView.effect = {
+            filter: {
+              timeExtent: timeSlider.timeExtent,
+              geometry: myview.extent
+            },
+            excludedEffect: "grayscale(20%) opacity(12%)"
+          };
+
  
 }); //require
