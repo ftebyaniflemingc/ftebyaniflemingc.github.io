11a8b93f761336eac67846595520d5737f18d5ac
ftebyaniflemingc <57412925+ftebyaniflemingc@users.noreply.github.com>
2020-06-11 08:08:38 -0400

    Update datalayer.js

--- durham_housing/app/datalayer.js
+++ durham_housing/app/datalayer.js
@@ -203,8 +203,8 @@ require([
             },
             outFields: ["*"],
             popupTemplate: {        // Enable a popup
-                  title: "{CensusBoundary2016_CTNAME}",       // Show attribute value
-                  content: "The census boundary has {infilling2019_csv_SumOfUnits} housing starts in {Date} ." 
+                  title: "{CTNAME}",       // Show attribute value
+                  content: "The census boundary has {SumOfUnits} housing starts in {Date} ." 
             }  // Display text in pop-up
           });//FeatureLayer
         }//tenLayer 
