5b8d6a47c315d71f4fa96f83f4055d32c1bd9642
christinelacanilao <57382843+christinelacanilao@users.noreply.github.com>
2020-06-11 15:29:56 -0400

    Update dojoConfig.js

--- durham_housing/dojoConfig.js
+++ durham_housing/dojoConfig.js
@@ -1,11 +1,9 @@
-
       var locationPath = location.pathname.replace(/\/[^\/]+$/, "");
       window.dojoConfig = {
-        
-        packages: [{
-            name: 'app',
-            location: locationPath + '/app'
-        
-          }]
+        packages: [
+          {
+            name: "app",
+            location: locationPath + "/app"
+          }
+        ]
       };
-
