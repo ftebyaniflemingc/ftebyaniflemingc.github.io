0dde65fd75413839a3edf1aa2746cda1b483753c
ftebyaniflemingc <57412925+ftebyaniflemingc@users.noreply.github.com>
2020-06-11 21:37:35 -0400

    Update HomeButton.js

--- durham_housing/app/HomeButton.js
+++ durham_housing/app/HomeButton.js
@@ -294,23 +294,26 @@ const fullTimeExtent = layer.timeInfo.fullTimeExtent;
         // wait till the layer view is loaded
         myview.whenLayerView(layer).then(function(lv) {
           layerView = lv;
-
+             
+              
           // start time of the time slider - 5/25/2019
           const start = new Date(2010, 1, 1);
+          const end: new Date(2019, 12, 31);
           // set time slider's full extent to
           // 5/25/5019 - until end date of layer's fullTimeExtent
           timeSlider.fullTimeExtent = {
             start: start,
-            end: layer.timeInfo.fullTimeExtent.end
+            end: end
+                //end: layer.timeInfo.fullTimeExtent.end
           };
 
           // We will be showing earthquakes with one day interval
           // when the app is loaded we will show earthquakes that
           // happened between 5/25 - 5/26.
-          const end = new Date(start);
+         // const end = new Date(start);
           // end of current time extent for time slider
           // showing earthquakes with one day interval
-          end.setDate(end.getDate() + 1);
+         // end.setDate(end.getDate() + 1);
 
           // Values property is set so that timeslider
           // widget show the first day. We are setting
@@ -322,8 +325,8 @@ const fullTimeExtent = layer.timeInfo.fullTimeExtent;
         timeSlider.watch("timeExtent", function() {
           // only show earthquakes happened up until the end of
           // timeSlider's current time extent.
-          layer.definitionExpression =
-            "time <= " + timeSlider.timeExtent.end.getTime();
+          //layer.definitionExpression =
+          //  "time <= " + timeSlider.timeExtent.end.getTime();
 
           // now gray out earthquakes that happened before the time slider's current
           // timeExtent... leaving footprint of earthquakes that already happened
