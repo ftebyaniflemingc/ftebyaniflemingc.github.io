55e2d07254466fe8f208756830ed6f573e1e214e
ftebyaniflemingc <57412925+ftebyaniflemingc@users.noreply.github.com>
2020-06-15 13:37:46 -0400

    Update datalayers.js

--- durham_housing/app/datalayers.js
+++ durham_housing/app/datalayers.js
@@ -175,8 +175,9 @@ require([
          xmax :  -78.790409,
          ymax : 43.868072
          }, 
-      });
-         /*  name : "Whitby" ,
+      }),
+         new Bookmark ({
+           name : "Whitby" ,
        extent : {
          spatialReference : {wkid : 32617},
         xmin : -78.880817,
@@ -184,7 +185,8 @@ require([
          xmax : -79.038958,
          ymax : 43.992001
       } */
-    ]
+         })
+     ]
      }) // other bookmarks ...   
    ]
 
