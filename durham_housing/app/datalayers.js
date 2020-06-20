3a5bc87a98a595d335eed9d188206676d1df0b33
ftebyaniflemingc <57412925+ftebyaniflemingc@users.noreply.github.com>
2020-06-20 13:01:53 -0400

    Update datalayers.js

--- durham_housing/app/datalayers.js
+++ durham_housing/app/datalayers.js
@@ -272,6 +272,6 @@ const bkExpand = new Expand ({
    content : bookmarks,
    expanded : false
 });
-         myview.ui.add (bkExpand, {position: "top-left", index: 4);
+         myview.ui.add (bkExpand, {position: "top-left", index: 4});
       
 }); //require
